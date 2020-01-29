9981593bba60643cb21e74ca61c9c17f78a89e34
Dexeat <Dexeat@users.noreply.github.com>
2020-01-29 16:52:12 +0100

    vb

--- game.js
+++ game.js
@@ -25,14 +25,13 @@ function init(){
     var platforms;
     var player;
     var cursors;
-    var cursors2;
-    var player2;
 }
 
 function preload(){
     this.load.image('background','assets/fond.png');
     this.load.image('sol','assets/sol.png');
-    this.load.spritesheet('perso','assets/enemies-spritesheet.png',{frameWidth: 20, frameHeight: 20});
+    this.load.spritesheet('perso','assets/32x32-bat-sprite.png',{frameWidth: 32, frameHeight: 32});
+    this.load.spritesheet('piece','assets/piece.png',{frameWidth: 16, frameHeight: 16});
 }
 function create(){
     this.add.image(400,300,'background');
@@ -43,55 +42,66 @@ function create(){
     platforms.create(600,250,'sol').setScale(0.25).refreshBody();
     platforms.create(400,400,'sol').setScale(0.2).refreshBody();
 
-    player = this.physics.add.sprite(100,0,'perso')/*.setScale(4).refreshBody()*/;
-    player2 = this.physics.add.sprite(200,0,'perso');
+    player = this.physics.add.sprite(100,0,'perso');
 
-
-    //-------------------------------------------------------------------------//
     player.setCollideWorldBounds(true);
-    player2.setCollideWorldBounds(true);
-
     this.physics.add.collider(player,platforms);
-    this.physics.add.collider(player2,platforms);
-    this.physics.add.collider(player,player2);
-
     player.setBounce(0.25);
-    player2.setBounce(0.25);
-    //player.body.setGravityY(300);
     cursors = this.input.keyboard.createCursorKeys();
-    cursors2 = this.cursors = this.input.keyboard.addKeys(
-        {up:Phaser.Input.Keyboard.KeyCodes.Z,
-        down:Phaser.Input.Keyboard.KeyCodes.S,
-        left:Phaser.Input.Keyboard.KeyCodes.Q,
-        right:Phaser.Input.Keyboard.KeyCodes.D});
-
 
     this.anims.create({
         key: 'gauche',
-        frames: this.anims.generateFrameNumbers('perso', {start: 0,end: 1}),
+        frames: this.anims.generateFrameNumbers('perso', {start: 13,end: 15}),
         frameRate:10
     });
 
     this.anims.create({
         key: 'droite',
-        frames: this.anims.generateFrameNumbers('perso', {start: 2,end: 3}),
+        frames: this.anims.generateFrameNumbers('perso', {start: 5,end: 7}),
         frameRate:10
+    });
+    
+    this.anims.create({
+        key: 'idle',
+        frames: [{key: 'perso', frame:0}],
+        frameRate:20
     });
 
     this.anims.create({
-        key: 'gauche2',
-        frames: this.anims.generateFrameNumbers('perso', {start: 16,end: 17}),
-        frameRate:10
+        key: 'fall',
+        frames: [{key: 'perso', frame:1}],
+        frameRate:20
     });
 
-    this.anims.create({
-        key: 'droite2',
-        frames: this.anims.generateFrameNumbers('perso', {start: 18,end: 19}),
-        frameRate:10
-    });
+    piece = this.add.group();
+
+    //  Now let's add 50 coins into it
+    for (var i = 0; i < 50; i++)
+    {
+        piece.create(this.world.randomX, this.world.randomY, 'piece', 0);
+    }
+
+
+    this.physics.add.collider(piece,platforms);
+    this.physics.add.overlap(player,piece,collectPiece,null,this);
+
+
+    //  Now using the power of callAll we can add the same animation to all coins in the group:
+    piece.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3], 10, true);
+
+    //  And play them
+    coins.callAll('animations.play', 'animations', 'spin');
+
 
 }
+
+function collectPiece(player, piece){
+    piece.disableBody(true,true);
+}
+
 function update(){
+
+
     if(cursors.left.isDown){
         player.setVelocityX(-100);
         player.anims.play('gauche', true);
@@ -100,24 +110,10 @@ function update(){
         player.anims.play('droite', true);
     } else {
         player.setVelocityX(0);
-        player.anims.play('gauche', true);
+        if(!player.body.touching.down){player.anims.play('fall', true);}
+        else{player.anims.play('idle', true);}
     }
     if(cursors.up.isDown/* && player.body.touching.down*/){
         player.setVelocityY(-500);
     }
-
-    if(cursors2.left.isDown){
-        player2.setVelocityX(-100);
-        player2.anims.play('gauche2', true);
-    } else if(cursors2.right.isDown){
-        player2.setVelocityX(100);
-        player2.anims.play('droite2', true);
-    } else {
-        player2.setVelocityX(0);
-        player2.anims.play('droite2', true);
-    }
-    if(cursors2.up.isDown /*&& player2.body.touching.down*/){
-        player2.setVelocityY(-500);
-    }
-
 }
